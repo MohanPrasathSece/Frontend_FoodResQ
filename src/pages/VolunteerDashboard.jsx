211ab32003bcc8b2cd33388404cab40e31695a00
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-05-09 12:12:39 +0530

    final

--- src/pages/VolunteerDashboard.jsx
+++ src/pages/VolunteerDashboard.jsx
@@ -327,7 +327,7 @@ export default function VolunteerDashboard() {
                     </CardBody>
                     <CardFooter>
                       <Button colorScheme="green" mr={2} onClick={() => handleDelivered(donation._id)}>Delivered</Button>
-                      <Button bg="red.500" color="white" size="sm" fontSize="xs" _hover={{ bg: 'red.600' }} onClick={() => handleNotDelivered(donation._id)}>Not Delivered</Button>
+                      <Button bg="red.500" color="white" _hover={{ bg: 'red.600' }} onClick={() => handleNotDelivered(donation._id)}>Not Delivered</Button>
                     </CardFooter>
                   </Card>
                 ))}
