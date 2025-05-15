7779f499a9844dbe713f1e690768584884ce8e7e
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-05-15 18:43:21 +0530

    final

--- src/pages/VolunteerDashboard.jsx
+++ src/pages/VolunteerDashboard.jsx
@@ -172,26 +172,12 @@ export default function VolunteerDashboard() {
 
   return (
     <Box
-      position="relative"
-      bgImage="url('https://1.bp.blogspot.com/-hGztsaeIFhY/Wi046IYRhwI/AAAAAAAAAJ0/fkZLk1ZSqCcBYLbaWy8WUGzrLlOlOXFgQCPcBGAYYCw/s1600/DSC_0097.JPG')"
-      bgSize="cover"
-      bgPosition="center"
+      bg="white"
       minH="100vh"
       width="100%"
       py={{ base: 4, md: 10 }}
       px={{ base: 2, md: 8 }}
       color={textColor}
-      _after={{
-        content: '""',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        bg: 'black',
-        opacity: 0.2,
-        zIndex: 0,
-      }}
     >
       <Container maxW={{ base: 'container.sm', md: '6xl' }} px={{ base: 2, md: 4 }} mx="auto" position="relative" zIndex={1}>
         <Stack spacing={8}>
